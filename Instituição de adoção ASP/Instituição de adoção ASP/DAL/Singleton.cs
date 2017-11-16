473a321cbc68672fbaa6e6a490379525431e25bf
Aluno <Aluno@050LABH037>
2017-11-16 09:03:01 -0200

    Criação da classe Usuario.Model

--- Instituição de adoção ASP/Instituição de adoção ASP/DAL/Singleton.cs
+++ Instituição de adoção ASP/Instituição de adoção ASP/DAL/Singleton.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Instituição_de_adoção_ASP;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Instituição_de_adoção_ASP.DAL
 {
-    public class Singleton
+    internal class Singleton
     {
+
+        private static readonly Singleton instance = new Singleton();
+        private readonly Entities entities;
+
+        private Singleton()
+        {
+            entities = new Entities();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public Entities Entities
+        {
+            get
+            {
+                return entities;
+            }
+        }
     }
 }
---
+++ Instituição de adoção ASP/Instituição de adoção ASP/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Instituição_de_adoção_ASP.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [MaxLength(50, ErrorMessage = " No máximo 50 caracteres")]
+        [Display(Name = "Nome de Usuario")]
+
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [MinLength(10, ErrorMessage = " No mínimo 10 caracteres")]
+        [MaxLength(10, ErrorMessage = " No máximo 10 caracteres")]
+        [Display(Name = "Telefone de Usuairio")]
+        public string Telefone { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [MinLength(5, ErrorMessage = " No mínimo 13 caracteres")]
+        [MaxLength(50, ErrorMessage = " No máximo 50 caracteres")]
+        [Display(Name = "Email de Usuario")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [MinLength(5, ErrorMessage = " No mínimo 5 caracteres")]
+        [MaxLength(50, ErrorMessage = " No máximo 50 caracteres")]
+        [Display(Name = "Endereço de Usuario")]
+        public string Endereco { get; set; }
+
+    }
+}
