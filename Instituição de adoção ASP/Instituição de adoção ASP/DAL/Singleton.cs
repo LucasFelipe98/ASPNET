55715638d0d54e1a65d7353ab91f9fa026c00cac
Aluno <Aluno@050LABH037>
2017-11-16 08:52:31 -0200

    Criação do Projeto + Singleton

---
+++ Instituição de adoção ASP/Instituição de adoção ASP/DAL/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instituição_de_adoção_ASP.DAL
+{
+    public class Singleton
+    {
+    }
+}
---
+++ Instituição de adoção ASP/Instituição de adoção ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Instituição_de_adoção_ASP.MvcApplication" Language="C#" %>
