6daea280ddc75350329d7a63e192943057836eea
Aluno <Aluno@050LABH037>
2017-11-21 08:56:43 -0200

    Criação da classe Entities . Model

---
+++ Instituição de adoção ASP/Instituição de adoção ASP/Models/Entities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Instituição_de_adoção_ASP.Models
+{
+    public class Entities : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Animal> Animais { get; set; }
+        public DbSet<Instituição> Instituição { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
